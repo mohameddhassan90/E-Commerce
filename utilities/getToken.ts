7c458bd13a7d482cbb7432d3d366462a1557ed94
mohameddhassan90 <mohameddhassan90@gmail.com>
2026-04-16 17:32:55 +0200

    third-comit

--- utilities/getToken.ts
+++ utilities/getToken.ts
@@ -7,9 +7,6 @@ export async function getTokenFn():Promise<string>{
     const nextAuthToken = 
     cookie.get("__Secure-next-auth.session-token")?.value||
     cookie.get(`next-auth.session-token`)?.value
-    if (!nextAuthToken) {
-    throw new Error("No session token found");
-  }
     const decodeCookie = await decode({secret:process.env.NEXTAUTH_SECRET! ,token:nextAuthToken})
     return decodeCookie?.token as string
 }
