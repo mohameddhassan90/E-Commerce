7082c8287ac65824ced918f2263243c3acf7d8c4
mohameddhassan90 <mohameddhassan90@gmail.com>
2026-04-16 17:50:20 +0200

    test

--- src/apis/cart/actions/addtocart.actions.ts
+++ src/apis/cart/actions/addtocart.actions.ts
@@ -5,7 +5,9 @@ import { getTokenFn } from "../../../../utilities/getToken";
 export async function addToCart(productId: string) {
     const token = await getTokenFn()
     if (!token)
-        throw new Error("Need To SignIn")
+    {
+        alert("Need To SignIn")
+    }
     try {
         const data = await fetch(`${process.env.API}cart`, {
             method: "POST",
